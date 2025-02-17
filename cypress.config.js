32a0a71babc48db8c954dc2de2054479d9539b2c
Rafael Campos <rafaelcampos@Rafaels-MacBook-Pro.local>
2025-02-17 17:01:40 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {}
+  e2e: {},
+  projectId: "u7vm8i"
   //video: true //quando roda em modo headles por default não salva mais o video
 })
